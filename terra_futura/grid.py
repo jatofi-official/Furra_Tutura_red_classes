6c6c7c5b6d3865ce84b756bc5056c448b2777bfa
Jatofi <jakubo.hlavaty2@gmail.com>
2025-12-02 11:36:52 +0100

    Implemented Grid.canPutCard and Grid.setActivated
    Started implementing Grid.state

--- terra_futura/grid.py
+++ terra_futura/grid.py
@@ -5,6 +5,8 @@ from terra_futura.simple_types import *
 class Grid (InterfaceGrid):
     def __init__(self) ->None:
 
+        self.gridSize = 3
+
         self._activationPattern: List[GridPosition] = []
         self._recentlyActivated: Set[GridPosition] = set()
         self._positions: Dict[GridPosition, InterfaceCard] = {}
@@ -18,8 +20,30 @@ class Grid (InterfaceGrid):
         return None
 
     def canPutCard(self, coordinate: GridPosition)-> bool:
-        return False
+        # First card can always be placed
+        if self._positions.keys() == []:
+            return True
+        
+        # Position occupied
+        if coordinate in self._positions.keys():
+            return False
+        
+        # get all x positions and y positions
+        xPos = [position.x for position in self._positions.keys()]
+        yPos = [position.y for position in self._positions.keys()]
 
+        xPos.append(coordinate.x)
+        yPos.append(coordinate.y)
+
+        width = max(xPos) - min(xPos) + 1
+        height = max(yPos) - min(yPos) + 1
+
+        #check if grid would change its max size
+        if width > self.gridSize or height > self.gridSize:
+            return False
+        
+        return True
+    
     def putCard(self, coordinate: GridPosition, card: InterfaceCard) -> bool:
         # If cannot put card return false (final check)
         if not self.canPutCard(coordinate):
@@ -42,7 +66,13 @@ class Grid (InterfaceGrid):
         return False
         
     def setActivated(self, coordinate: GridPosition) -> None:
-        ...
+        # I don't understand reason why putCard can return boolean but setActivated cannot,
+        # when it works the same principle. Unfortunately it is not possible at this moment
+        # to change all interfaces so I have to raise an error...
+        if not self.canBeActivated(coordinate):
+            raise ValueError("Unable to activate on position")
+        
+        self._recentlyActivated.add(coordinate)
 
     def setActivationPattern(self, pattern: List[GridPosition]) -> None:
         # Set pattern
@@ -55,4 +85,8 @@ class Grid (InterfaceGrid):
         self._recentlyActivated.clear()
 
     def state(self) -> str:
+        if not self._positions.keys():
+            return "Empty grid"
+
+
         return ""
